4ecf80b5b11da61a8a76fd96bf20ee744ceba78c
Lily Geidelberg <Geidelberg@users.noreply.github.com>
2021-06-06 12:18:28 +0100

    bash files

---
+++ bash/date_trees_climb_B.1.1.7_control.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+#SBATCH --cpus-per-task=32
+#SBATCH --job-name=compare_lineages_d1_B.1.1.7_control
+#SBATCH --output=compare_lineages_d1_B.1.1.7_control.out
+#SBATCH --account=lomannj-covid-19-realtime-epidemiology
+#SBATCH --qos=lomannj
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1
+eval "$(conda shell.bash hook)"
+conda activate r-environment
+Rscript /cephfs/covid/bham/climb-covid19-geidelbergl/R/compare_lineages_d1_B.1.1.7_control.R
---
+++ bash/mlesky_climb_B.1.617.2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+#SBATCH --cpus-per-task=32
+#SBATCH --job-name=mlesky_d1_B.1.617.2
+#SBATCH --output=mlesky_d1_B.1.617.2.out
+#SBATCH --account=lomannj-covid-19-realtime-epidemiology
+#SBATCH --qos=lomannj
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1
+eval "$(conda shell.bash hook)"
+conda activate r-environment
+Rscript /cephfs/covid/bham/climb-covid19-geidelbergl/R/mlesky_d1_B.1.617.2.R
